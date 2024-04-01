419f265152517c821005a0f3700379e0f7556df2
David Ley <starburstx64@gmail.com>
2024-04-01 13:01:00 -0600

    feat: Change telescope <leader>. -> <leader>,

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
   },
   keys = {
     {
-      "<leader>.",
+      "<leader>,",
       "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
       desc = "Switch Buffer",
     },
@@ -45,11 +45,6 @@ return {
       "<leader>fb",
       "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
       desc = "Buffers",
-    },
-    {
-      "<leader>fg",
-      "<cmd>Telescope git_files<cr>",
-      desc = "Find Files (git-files)",
     },
     { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
     -- git
