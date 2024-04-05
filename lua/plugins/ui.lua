1359a09769809b006ab51bac70da88d215187aff
David Ley <starburstx64@gmail.com>
2024-04-05 11:23:23 -0600

    Add incline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -143,4 +143,41 @@ return {
       },
     },
   },
+  {
+    "b0o/incline.nvim",
+    opts = {
+      hide = {
+        cursorline = true,
+        focused_win = true,
+      },
+      window = {
+        padding = 0,
+        margin = { horizontal = 0 },
+      },
+      render = function(props)
+        local helpers = require("incline.helpers")
+        local filename =
+          vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        local ft_icon, ft_color =
+          require("nvim-web-devicons").get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        local buffer = {
+          ft_icon and {
+            " ",
+            ft_icon,
+            " ",
+            guibg = ft_color,
+            guifg = helpers.contrast_color(ft_color),
+          } or "",
+          " ",
+          { filename, gui = modified and "bold,italic" or "bold" },
+          " ",
+          guibg = "#44406e",
+        }
+        return buffer
+      end,
+    },
+    -- Optional: Lazy load Incline
+    event = { "BufReadPre", "BufNewFile" },
+  },
 }
