0724e8520fe8faf93c8dc39979afb00e32c05a5e
starb <starburstx64@gmail.com>
2024-09-11 13:14:54 -0600

    install no-clown-fiesta colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,18 @@
 return {
   {
+    "no-clown-fiesta/no-clown-fiesta.nvim",
+    priority = 1000,
+    config = function(_, opts)
+      require("no-clown-fiesta").setup(opts)
+      vim.cmd([[colorscheme no-clown-fiesta]])
+    end,
+  },
+
+  {
     "catppuccin/nvim",
     priority = 1000,
     name = "catppuccin",
+    enabled = false,
     opts = {
       term_colors = false,
       transparent_background = false,
