54374d9fad14baebd1789cbab2eb127b56bd7045
David Alberto Segovia Ley <dsegovia@frd.org.mx>
2024-10-17 11:38:55 -0600

    lsp-zero

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,244 +1,158 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    "VonHeikemen/lsp-zero.nvim",
     dependencies = {
+      "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-
       "hrsh7th/cmp-nvim-lsp",
-      { "folke/lazydev.nvim", opts = {} },
     },
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
+    branch = "v4.x",
     config = function()
-      local keymap = vim.keymap
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup(
-          "starb-lsp-attach",
-          { clear = true }
-        ),
-        callback = function(_)
-          local opts = { noremap = true, silent = true }
-
-          -- Set keybindings
-          opts.desc = "Show LSP references"
-          keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-          opts.desc = "Go to declaration"
-          keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-          opts.desc = "Show LSP definitions"
-          -- stylua: ignore
-          keymap.set("n", "gd",
-            function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,
-            opts) -- show lsp definitions
-
-          opts.desc = "Show LSP implementations"
-          -- stylua: ignore
-          keymap.set("n", "gi",
-            function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
-            opts) -- show lsp implementations
-
-          opts.desc = "Show LSP type definitions"
-          -- stylua: ignore
-          keymap.set("n", "gt",
-            function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end,
-            opts) -- show lsp type definitions
-
-          opts.desc = "See available code actions"
-          keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-          opts.desc = "Rename"
-          keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts) -- smart rename
-
-          opts.desc = "Show buffer diagnostics"
-          keymap.set(
-            "n",
-            "<leader>cD",
-            "<cmd>Telescope diagnostics bufnr=0<CR>",
-            opts
-          ) -- show  diagnostics for file
-        end,
-      })
-
-      -- used to enable autocompletion (assign to every lsp server config)
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend(
-        "force",
-        capabilities,
-        require("cmp_nvim_lsp").default_capabilities()
-      )
-
-      local signs =
-        { Error = " ", Warn = " ", Hint = " ", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
-      local servers = {
-        html = {},
-        clangd = {},
-        ts_ls = {
-          init_options = {
-            plugins = {
-              {
-                -- Vue
-                -- NOTE: location can be global or nothing if it points to the
-                -- local node_modules of the project
-                name = "@vue/typescript-plugin",
-                location = "",
-                languages = { "javascript", "typescript", "vue" },
-              },
-            },
-          },
-          filetypes = {
-            "javascript",
-            "typescript",
-            "vue",
-          },
-          on_attach = function(client)
-            client.server_capabilities.documentFormattingProvider = false
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+        handlers = {
+          function(server_name, _)
+            require("lspconfig")[server_name].setup({})
           end,
-        },
-        cssls = {},
-        eslint = {
-          useFlatConfig = false,
-          on_attach = function(_, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              command = "EslintFixAll",
+
+          ruff = function()
+            require("lspconfig").ruff.setup({
+              cmd_env = { RUFF_TRACE = "messages" },
+              init_options = {
+                settings = {
+                  logLevel = "error",
+                },
+              },
+              on_attach = function(client, _)
+                client.server_capabilities.hoverProvider = false
+              end,
+            })
+          end,
+
+          basedpyright = function()
+            require("lspconfig").basedpyright.setup({
+              settings = {
+                basedpyright = {
+                  analysis = {
+                    typeCheckingMode = "standard",
+                  },
+                },
+              },
+            })
+          end,
+
+          eslint = function()
+            require("lspconfig").eslint.setup({
+              settings = {
+                basedpyright = {
+                  analysis = {
+                    typeCheckingMode = "standard",
+                  },
+                },
+              },
             })
           end,
         },
-        lua_ls = {
-          settings = { -- custom settings for lua
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-              },
-              workspace = {
-                -- make language server aware of runtime files
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.stdpath("config") .. "/lua"] = true,
+
+        ts_ls = function()
+          require("lspconfig").ts_ls.setup({
+            init_options = {
+              plugins = {
+                {
+                  -- Vue
+                  -- NOTE: location can be global or nothing if it points to the
+                  -- local node_modules of the project
+                  name = "@vue/typescript-plugin",
+                  location = "",
+                  languages = { "javascript", "typescript", "vue" },
                 },
               },
             },
-          },
-        },
-        gdscript = {
-          -- NOTE: This only works for windows
-
-          -- Put this flags in godot
-          -- Use External Editor: On
-          -- Exec Path: nvim
-          -- Exec Flags: --server "\\\\.\\pipe\\godot.pipe" --remote-send "<C-\><C-N>:n {file}<CR>:call cursor({line},{col})<CR>"
-          cmd = { "ncat", "localhost", "6005" },
-
-          --cmd = vim.lsp.rpc.connect("172.19.240.1", 6005),
-          root_dir = function()
-            return vim.fs.dirname(
-              vim.fs.find({ "project.godot", ".git" }, { upward = true })[1]
-            )
-          end,
-          on_attach = function()
-            local pipe = [[\\.\pipe\godot.pipe]]
-            if vim.fn.has("win32") == 0 then
-              pipe = "/tmp/godot.pipe"
-            end
-
-            local success = pcall(
-              vim.api.nvim_command,
-              [[echo serverstart(']] .. pipe .. [[')]]
-            )
-
-            if success then
-              print("Godot Language Server Connected")
-            end
-          end,
-        },
-        volar = {
-          on_attach = function(client)
-            client.server_capabilities.documentFormattingProvider = false
-          end,
-        },
-        -- vuels = {},
-        yamlls = {},
-        gopls = {
-          gofumpt = true,
-        },
-
-        -- python
-        ruff = {
-          cmd_env = { RUFF_TRACE = "messages" },
-          init_options = {
-            settings = {
-              logLevel = "error",
+            filetypes = {
+              "javascript",
+              "typescript",
+              "vue",
             },
-          },
-          on_attach = function(client, _)
-            client.server_capabilities.hoverProvider = false
-          end,
-        },
-
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              analysis = {
-                typeCheckingMode = "standard",
-              },
-            },
-          },
-        },
-      }
-
-      local ensure_installed = {
-        "volar",
-        "ts_ls",
-        "html",
-        "eslint",
-        "cssls",
-        "clangd",
-        "lua_ls",
-
-        "stylua", -- lua formatter
-
-        -- debuggers
-        "js-debug-adapter",
-      }
-
-      require("mason-tool-installer").setup({
-        ensure_installed = ensure_installed,
+            on_attach = function(client)
+              client.server_capabilities.documentFormattingProvider = false
+            end,
+          })
+        end,
       })
 
-      require("mason-lspconfig").setup({
-        automatic_installation = false, -- not the same as ensure_installed
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_extend(
-              "force",
-              {},
-              vim.deepcopy(capabilities),
-              server.capabilities or {}
-            )
-          end,
-        },
-      })
-
-      for server_name, config in pairs(servers) do
-        require("lspconfig")[server_name].setup(config)
+      local lsp_attach = function(_, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+          --stylua: ignore start
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set({'n', 'v'}, '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+          vim.keymap.set("n", "<leader>cD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+        --stylua: ignore end
       end
-    end,
-  },
 
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    event = "VeryLazy",
-    config = true,
+      local lsp_zero = require("lsp-zero")
+      local lua_opts = lsp_zero.nvim_lua_ls()
+
+      require("lspconfig").lua_ls.setup(lua_opts)
+
+      lsp_zero.configure("gdscript", {
+        -- NOTE: This only works for windows
+
+        -- Put this flags in godot
+        -- Use External Editor: On
+        -- Exec Path: nvim
+        -- Exec Flags: --server "\\\\.\\pipe\\godot.pipe" --remote-send "<C-\><C-N>:n {file}<CR>:call cursor({line},{col})<CR>"
+        cmd = { "ncat", "localhost", "6005" },
+
+        --cmd = vim.lsp.rpc.connect("172.19.240.1", 6005),
+        root_dir = function()
+          return vim.fs.dirname(
+            vim.fs.find({ "project.godot", ".git" }, { upward = true })[1]
+          )
+        end,
+        on_attach = function()
+          local pipe = [[\\.\pipe\godot.pipe]]
+          if vim.fn.has("win32") == 0 then
+            pipe = "/tmp/godot.pipe"
+          end
+
+          local success = pcall(
+            vim.api.nvim_command,
+            [[echo serverstart(']] .. pipe .. [[')]]
+          )
+
+          if success then
+            print("Godot Language Server Connected")
+          end
+        end,
+      })
+
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+
+      lsp_zero.ui({
+        float_border = "rounded",
+        sign_text = {
+          error = " ",
+          warn = " ",
+          hint = " ",
+          info = " ",
+        },
+      })
+    end,
   },
 }
