6f511f3702468333123cb82596ea389f502c5b27
starb <starburstx64@gmail.com>
2024-11-25 15:25:23 -0600

    switch dashboard to alpha

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,8 +1,10 @@
 return {
-  "nvimdev/dashboard-nvim",
+  "goolord/alpha-nvim",
+  dependencies = { "echasnovski/mini.icons" },
   event = "VimEnter",
-  enabled = true,
-  opts = function()
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
     local logo = [[
       ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
       ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
@@ -20,38 +22,7 @@ return {
       ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
     ]]
 
-    logo = string.rep("\n", 1) .. logo .. "\n"
-
-    local opts = {
-      theme = "hyper",
-      hide = {
-        -- this is taken care of by lualine
-        -- enabling this messes up the actual laststatus setting after loading a file
-        statusline = false,
-      },
-      config = {
-        header = vim.split(logo, "\n"),
-        weak_header = {
-          enable = false,
-        },
-        footer = {},
-        project = {
-          enable = false,
-        },
-        mru = {
-          enable = true,
-        },
-        shortcut = {
-          {
-            desc = " Continue",
-            group = "DiagnosticHint",
-            action = 'lua require("persistence").load()',
-            key = "c",
-          },
-        },
-      },
-    }
-
-    return opts
+    dashboard.section.header.val = vim.split(logo, "\n")
+    alpha.setup(dashboard.opts)
   end,
 }
