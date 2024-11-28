ad7fec461a057b5c283e7b1244ce18064165bb3e
starb <starburstx64@gmail.com>
2024-11-27 18:53:08 -0600

    use default timeoutlen

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,8 +23,6 @@ end
 
 local opt = vim.opt
 
-opt.timeoutlen = 300
-
 opt.mouse = "a" -- enable mouse
 opt.termguicolors = true -- True color support
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
