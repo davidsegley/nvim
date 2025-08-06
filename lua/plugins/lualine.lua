2c4a6480f743a2d05141479cc21db29c35203689
starb <starburstx64@gmail.com>
2025-08-05 18:21:50 -0600

    disable lualine lol

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    enabled = true,
+    enabled = false,
     opts = function()
       return {
         options = {
