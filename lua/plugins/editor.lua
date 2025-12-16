c0e50037d526ccfeddf31c562632accbe1edf1a7
David Segovia <davidsegovia@google.com>
2025-12-16 10:59:26 -0600

    nvim osc52 for ssh sessions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -142,4 +142,26 @@ return {
       return keys
     end,
   },
+
+  {
+    "ojroques/nvim-osc52",
+    enabled = vim.env.SSH_CONNECTION ~= nil,
+    config = function()
+      require("osc52").setup({
+        max_length = 0, -- Maximum length of selection (0 for no limit)
+        silent = false, -- Disable message on successful copy
+        trim = false, -- Trim surrounding whitespaces before copy
+      })
+      local function copy()
+        if
+          (vim.v.event.operator == "y" or vim.v.event.operator == "d")
+          and vim.v.event.regname == "+"
+        then
+          require("osc52").copy_register("+")
+        end
+      end
+
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+    end,
+  },
 }
