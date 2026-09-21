249a43081cd9aa5bd06c49c5bccb76f3ea776498
David Segovia <davidsegovia@google.com>
2026-09-21 22:36:03 +0000

    changes

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ require("config.keymaps")
 require("config.autocmds")
 require("config.lsp")
 require("config.plugins")
+
+local ok, local_config = pcall(require, "local")
+if ok and type(local_config) == "table" and local_config.setup then
+  local_config.setup()
+end
